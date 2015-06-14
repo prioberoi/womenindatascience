93d71cd7d796073fa88058dd9fd3d860fd583060
prioberoi <prioberoi@gmail.com>
2015-06-14 06:39:58 -0400

    adding new data visualization

--- server.R
+++ server.R
@@ -1,3 +1,6 @@
+#if("shiny" %in% rownames(installed.packages()) == FALSE) {install.packages("shiny")}
+#if("googleVis" %in% rownames(installed.packages()) == FALSE) {install.packages("googleVis")}
+
 library(shiny)
 library(googleVis)
 
@@ -8,34 +11,35 @@ shinyServer(
     data1.t <- t(data1)
     colnames(data1.t) = data1.t[1,]
     data1.t.2 <- as.data.frame(data1.t[2:11,],stringsAsFactors=FALSE)
-    
+  
     data2 <- read.csv("tab9-9.1.csv",header=TRUE,stringsAsFactors=FALSE)
     
-    output$lineGraph <- renderGvis({
+    output$dashboard <- renderGvis({
       year <- as.numeric(c("2004","2005","2006","2007","2008","2009","2010","2011","2012","2013"))
       data1.t.2 <- cbind(year,data1.t.2)
-      data1.t.2[,2:25] <- lapply(data1.t.2[,2:25],function(data1.t.2){as.numeric(gsub(",", "", data1.t.2))})
-      graph <- gvisLineChart(data1.t.2,xvar="year",yvar=c("All occupations","Management, professional, and related occupations","Management, business, and financial operations occupations",
-                                                          "Professional and related occupations","Biological and life scientist","Dietitian","Economist and market and survey researcher",
-                                                          "Engineering and related technologist or technician","Engineer","Health technologist or technician","Lawyer or judge",
-                                                          "Mathematical or computer scientist","Pharmacist","Physician","Psychologist","Registered nurse","Science technician",
-                                                          "Teacher, except postsecondary (college and university)","Teacher, postsecondary (college and university)","Therapist",
-                                                          "Other health care professional","Other natural scientist","Other professional or related occupation","Other occupations"),
-                             options=list(height=750,
-                                          chartArea="{left:100,top:50,width:'65%',height:'85%'}",
-                                          hAxis="{title:'Year',format:'####'}",
-                                          vAxis="{title:'Number of Women, By Occupation'}",
-                                          title="Women in Data Science"))
-      graph
+      data1.t.2[,2:20] <- lapply(data1.t.2[,2:20],function(data1.t.2){as.numeric(gsub(",", "", data1.t.2))})
+      
+      chart <- gvisBubbleChart(data2,idvar="Category",xvar="Employed",yvar="Not.Employed",sizevar="Total",colorvar="Series",
+                               options=list(width=1800, height=800,
+                                            titlePosition="out",
+                                            chartArea="{width:'100%',height:'80%', legend: {position: 'in'}}",
+                                            sizeAxis='{minSize: 20, maxSize: 150}',
+                                            colorAxis="{minValue: 0, colors: ['#490A3D','#BD1550','#E97F02','#F8CA00','#8A9B0F']}",
+                                            hAxis="{title:'Total Employed',minValue:-5000000}", #,maxValue:9000000
+                                            vAxis="{title:'Total Unemployed',minValue:-500000}", #
+                                            title="Employment status of scientists and engineers, by sex, ethnicity, race, and disability status: 2013",
+                                            titleTextStyle="{color:'#490A3D', fontName:'calibri', fontSize:40}",
+                                            bubble="{
+                                              textStyle:{
+                                                color: 'black', fontName: 'Calibri'},
+                                              opacity:'0.3'}",
+                                            explorer="{actions: ['dragToZoom','rightClickToReset'],maxZoomIn:0.05}"))
+      
+      output <- chart
+      
+      output
     })
     
-    output$bubbleChart <- renderGvis({
-      chart <- gvisBubbleChart(data2,idvar="Category",xvar="Not.Employed",yvar="Employed",sizevar="Total",
-                               options=list(height=750,
-                                            hAxis="{title:'Total Unemployed',minValue:-2500000,maxValue:9500000}",
-                                            vAxis="{title:'Total Employed',minValue:-20000000}",
-                                            title="Employment status of scientists and engineers, by sex, ethnicity, race, and disability status: 2013"))
-      chart
-    })
+
   }
 )
--- ui.R
+++ ui.R
@@ -1,11 +1,12 @@
 shinyUI(navbarPage("Women in Data Science",
   
-  tabPanel("Line Graph",
-    htmlOutput("lineGraph")
-  ),
+  tabPanel("Dashboard",
   
-  tabPanel("Bubble Chart",
-    htmlOutput("bubbleChart")
+    htmlOutput("dashboard", align="center"),
+    div(img(src="stat_1_3.png"), img(src="stat_2_1.png"), img(src="stat_3.png"), align="center", img(src="logo_060815.jpeg", width=300, align="right"))
+    #HTML('<div><img hspace="100" src="stat_1_3.png" hspace="75"/><img src="stat_2_1.png" hspace="75"/><img src="stat_3.png"/></p>'),
+    #img(src="logo_060815.jpeg", width=300, align="right")
+    #p('National Science Foundation, National Center for Science and Engineering Statistics. 2015. Women, Minorities, and Persons with Disabilities in Science and Engineering: 2015. Special Report NSF 15-311. Arlington, VA. Available at http://www.nsf.gov/statistics/wmpd/.')
   )
   
 ))
